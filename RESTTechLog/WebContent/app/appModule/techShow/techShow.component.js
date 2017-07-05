6807020561032e5f1e193e94ff1365e6f036e442
ntran1321 <nancy.tran.1321@gmail.com>
2017-07-05 08:36:56 -0600

    added css

---
+++ RESTTechLog/WebContent/app/appModule/techShow/techShow.component.js
@@ -0,0 +1,13 @@
+angular.module('appModule').component('techShow', {
+	templateUrl : 'app/appModule/techShow/techShow.component.html',
+	controller : function() {
+		
+		var vm = this;
+		
+		
+	},
+	
+	controllerAs : vm;
+	
+
+});
